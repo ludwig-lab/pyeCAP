81a77d76b29e3872198113f48f7c1ab85cb95915
mlaluzerne <65750395+mlaluzerne@users.noreply.github.com>
2021-05-13 10:55:59 -0500

    Update conf.py
    Update documentation configuration.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -30,7 +30,7 @@ release = '0.0.1'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.autodoc','sphinx.ext.coverage','sphinx.ext.napoleon','nbsphinx','sphinx_rtd_theme',
+extensions = ['sphinx.ext.autodoc','sphinx.ext.coverage','sphinx.ext.napoleon','sphinx_rtd_theme',
               'sphinx.ext.doctest', 'sphinx.ext.autosectionlabel'
 ]
 
