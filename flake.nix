9128477dc3d1d6643f4180a369628305a2723bb8
James Trevathan <james.trevathan@gmail.com>
2026-04-21 11:32:18 -0500

    adding pythonpath setup

--- flake.nix
+++ flake.nix
@@ -187,6 +187,7 @@
               # Get repository root using git. This is expanded at runtime by the editable `.pth` machinery.
               export REPO_ROOT=$(git rev-parse --show-toplevel)
               export PATH=$PATH:$(git rev-parse --show-toplevel)
+              export PYTHONPATH=$PYTHONPATH:$(git rev-parse --show-toplevel)
               python -m ipykernel install --name=pyecap-dev-env
             '';
           };
